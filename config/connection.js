23e972ac06f8c556a88f8865743c54bb720a06b4
JacobBraxley <jacobbraxley@gmail.com>
2019-06-05 16:05:58 -0400

    progess attempt

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "192.168.99.103",
-  port: 3306,
-  user: "root",
-  password: "docker",
-  database: "burgers_db"
+const { Client } = require('pg');
+
+const connection = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+// const connection = mysql.createConnection({
+//   host: process.env.HOSTNAME || "ec2-50-19-114-27.compute-1.amazonaws.com",
+//   port: process.env.PORT || 5432,
+//   user: "rfyskftunwmnja",
+//   password: "a3aad821d7ebb07162b99f3ebdce3d9c588027a39cf1788581a543875c026b3a",
+//   database: "d1ihk90kca33c1"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
